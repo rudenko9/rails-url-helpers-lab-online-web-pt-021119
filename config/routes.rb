e7245283297b54978515314bbcc7ed9e82568c73
Nataliya R <n.rudenko@gmail.com>
2019-07-04 20:14:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :show]
-  get '/students/:id/activate', to: 'studetns#avtivate', as:'activate_student'
+
+
+
+
+
+get '/students/:id/activate', to: 'studetns#avtivate', as:'activate_student'
 end
