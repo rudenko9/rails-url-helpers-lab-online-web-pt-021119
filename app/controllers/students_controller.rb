a03bd90c3cfb1ca40c4c6123f4acf69056833600
Nataliya R <n.rudenko@gmail.com>
2019-07-04 20:09:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,12 @@ class StudentsController < ApplicationController
     
   end
   
+  def activate
+    student = Student.find(params[:id])
+    student.active = !student.active
+    student.save
+    redirect_to student_path(params[:id])
+  end
 
   private
 
