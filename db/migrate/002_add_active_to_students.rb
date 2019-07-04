c69df9a684fa06699ca3726dd6d0e254d3e4e814
Nataliya R <n.rudenko@gmail.com>
2019-07-04 19:18:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,10 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
+    add column :students do |t|
+      boolean :active
       
-      t.timestamps null: false
+      
+      
     end
   end
 end
